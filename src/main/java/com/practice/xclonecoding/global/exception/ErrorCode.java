7540633e8b6e58375718cbc46180e98e8d5c3de7
corjqnrl <coqnrl@ewha.ac.kr>
2026-05-19 13:24:14 +0900

    feat: global error code 생성

---
+++ src/main/java/com/practice/xclonecoding/global/exception/ErrorCode.java
@@ -0,0 +1,22 @@
+package com.practice.xclonecoding.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    INTERNAL_SERVER_ERROR(500, "서버 에러가 발생했습니다."),
+    ERROR(400, "요청 처리에 실패했습니다."),
+
+    // account
+    ACCOUNT_NOT_FOUND(404, "존재하지 않는 계정입니다."),
+
+    // post
+    POST_NOT_FOUND(404, "존재하지 않는 게시물입니다."),
+    POST_ACCOUNT_MISMATCH(401, "게시글 생성자가 아닙니다.");
+
+    private final int status;
+    private final String message;
+}
