322180f8d26d19fac687a21558853b133842545f
corjqnrl <coqnrl@ewha.ac.kr>
2026-05-19 12:45:34 +0900

    feat: initial project setup

---
+++ src/main/java/com/practice/xclonecoding/XclonecodingApplication.java
@@ -0,0 +1,13 @@
+package com.practice.xclonecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XclonecodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XclonecodingApplication.class, args);
+	}
+
+}
