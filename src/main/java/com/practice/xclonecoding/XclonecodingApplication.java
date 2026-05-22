e973c2fdbfff6c834076281e6a54451a8f3709cb
corjqnrl <coqnrl@ewha.ac.kr>
2026-05-22 23:38:55 +0900

    fix: 메인 어플리케이션 클래스 수정

--- src/main/java/com/practice/xclonecoding/XclonecodingApplication.java
+++ src/main/java/com/practice/xclonecoding/XclonecodingApplication.java
@@ -2,12 +2,12 @@ package com.practice.xclonecoding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class XclonecodingApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(XclonecodingApplication.class, args);
 	}
-
 }
