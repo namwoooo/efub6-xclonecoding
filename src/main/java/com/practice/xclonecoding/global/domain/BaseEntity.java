fa04848d8433440b61c71490ef161c4ab41c3215
corjqnrl <coqnrl@ewha.ac.kr>
2026-05-22 23:39:13 +0900

    feat: base entity 생성

--- src/main/java/com/practice/xclonecoding/global/domain/BaseEntity.java
+++ src/main/java/com/practice/xclonecoding/global/domain/BaseEntity.java
@@ -2,6 +2,7 @@ package com.practice.xclonecoding.global.domain;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
